c56268b3abf4acfdc293f9d444caab21866f3d98
Guka <guramgunia@gmail.com>
2019-11-11 18:00:05 +0400

    feat: added placeholder for input

--- src/components/EmbeddedDocument.jsx
+++ src/components/EmbeddedDocument.jsx
@@ -16,10 +16,10 @@ const EmbeddedDocument = () => {
       ref={inputRef}
       style={{ fontSize: 20, color: 'black', width: '100%', outline: 'none', border: 'none' }}
       autoFocus
+      placeholder="Paste a link to embed content from another site and press enter"
       onBlur={() => toggleEditor(true)}
     />
   )
 }
-
 
 export default EmbeddedDocument
