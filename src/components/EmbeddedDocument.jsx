4881e63bc016cb6a0f1e5475d3340fdded91ebdd
Guka <guramgunia@gmail.com>
2019-11-11 18:01:52 +0400

    refactor: move inline styles to class

--- src/components/EmbeddedDocument.jsx
+++ src/components/EmbeddedDocument.jsx
@@ -14,7 +14,7 @@ const EmbeddedDocument = () => {
   return (
     <input
       ref={inputRef}
-      style={{ fontSize: 20, color: 'black', width: '100%', outline: 'none', border: 'none' }}
+      className="EmbedInput"
       autoFocus
       placeholder="Paste a link to embed content from another site and press enter"
       onBlur={() => toggleEditor(true)}
